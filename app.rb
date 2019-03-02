0e5377b2d21c4e6bf25e9d4e4fc456424dfc29a9
Stef Claus <stef.claus@gmail.com>
2019-03-02 23:16:19 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Stef Claus."
+  end
+
+  get '/hometown' do
+    "My hometown is Craig, Alaska."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 4am."
+  end
+
 end
