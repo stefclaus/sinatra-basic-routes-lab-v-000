2f02b310579f6413f7785cb2a7b79532f2150efa
Stef Claus <stef.claus@gmail.com>
2019-03-02 23:14:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+  @medicines = Medicine.all
+
+  get '/hometown' do
+  @medicines = Medicine.all
+
+  get '/favorite-song' do
+  @medicines = Medicine.all
+
 end
